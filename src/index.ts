0c2ec8f1f9b4bb443e580b035e1a353ff2959619
Gerardo Pérez <gerard2perez@outlook.com>
2019-05-29 14:27:14 -0500

    fix: correctly require the configuration

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import { KaenContext } from '@kaenjs/core';
 import { configuration } from '@kaenjs/core/configuration';
 import * as _session from 'express-session';
-const { authentication: { Keys, Session } } = configuration;
-const options = Object.assign({}, {secret:Keys}, Session);
-const session = _session(options);
+const { server: { Keys }, session } = configuration;
+const options = Object.assign({}, {secret:Keys}, session);
+const sessionInstance = _session(options);
 async function _Session (ctx: KaenContext) {
 	await new Promise(resolve => {
 		//@ts-ignore
-		session(ctx.req, ctx.res, resolve);
+		sessionInstance(ctx.req, ctx.res, resolve);
 	});
 };
 export { _Session as Session };
