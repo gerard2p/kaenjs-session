bf29aec486a1f1b374f4a5146c0af26e42be2d70
Gerardo Pérez <gerard2perez@outlook.com>
2019-05-29 14:05:47 -0500

    fix: session will only create one instance

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,12 @@ import { KaenContext } from '@kaenjs/core';
 import { configuration } from '@kaenjs/core/configuration';
 import * as _session from 'express-session';
 const { authentication: { Keys, Session } } = configuration;
-
-function _Session() {
-	let options = Object.assign({}, {secret:Keys}, Session);
-	let session = _session(options);
-	return async function (ctx: KaenContext) {
-		await new Promise(resolve => {
-			//@ts-ignore
-			session(ctx.req, ctx.res, resolve);
-		});
-	};
-}
+const options = Object.assign({}, {secret:Keys}, Session);
+const session = _session(options);
+async function _Session (ctx: KaenContext) {
+	await new Promise(resolve => {
+		//@ts-ignore
+		session(ctx.req, ctx.res, resolve);
+	});
+};
 export { _Session as Session };
